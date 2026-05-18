311d18f56b481bb3b85aa5b985e29ea92c69738f
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-05-18 14:43:37 +0700

    menambahkan warisan dari cl;ass seseorang

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -4,5 +4,13 @@ using namespace std;
 class seseorang
 {
 public:
-    virtual void pesan() = 0
+    virtual void pesan() = 0;
+    virtual void pesan()
+    {
+        cout << "Pesan dari seseorang" << endl;
+    }
+};
+
+class joko : public seseorang
+{
 }
